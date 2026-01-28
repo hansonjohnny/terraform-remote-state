cc080f49c6cc0b14de345e643146907b2133cdef
Johnny Hanson <hansonjohnny>
2026-01-28 10:30:20 +0000

    Remove duplicate AMI data source from main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -86,21 +86,21 @@ provider "aws" {
   }
 }
 
-# Get the latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
+# # Get the latest Amazon Linux 2 AMI
+# data "aws_ami" "amazon_linux_2" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
 # Get the default VPC
 data "aws_vpc" "default" {
